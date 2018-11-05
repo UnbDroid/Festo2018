927a7f7158cb089d59dcb00dfc6b6301da1982fe
Matheus Feitosa de Castro <matheus@Matheuss-MacBook-Air.local>
2018-11-05 20:41:27 -0300

    add teste com opencv

--- catkin_festo2018/src/vision/src/vision_node.cpp
+++ catkin_festo2018/src/vision/src/vision_node.cpp
@@ -30,6 +30,29 @@ class ImageConverter {
     ~ImageConverter() { cv::destroyWindow(OPENCV_WINDOW); }
 
     void imageCb(const sensor_msgs::ImageConstPtr& msg) {
+        int iLowH = 170;
+        int iHighH = 179;
+
+        int iLowS = 150; 
+        int iHighS = 255;
+
+        int iLowV = 60;
+        int iHighV = 255;
+
+        createTrackbar("LowH", "Control", &iLowH, 179); //Hue (0 - 179)
+        createTrackbar("HighH", "Control", &iHighH, 179);
+
+        createTrackbar("LowS", "Control", &iLowS, 255); //Saturation (0 - 255)
+        createTrackbar("HighS", "Control", &iHighS, 255);
+
+        createTrackbar("LowV", "Control", &iLowV, 255);//Value (0 - 255)
+        createTrackbar("HighV", "Control", &iHighV, 255);
+
+        int iLastX = -1; 
+        int iLastY = -1;
+
+        Mat imgTmp;
+
         cv_bridge::CvImagePtr cv_ptr;
         try {
             cv_ptr =
@@ -39,19 +62,87 @@ class ImageConverter {
             return;
         }
         // Draw an example circle on the video stream
-        if (cv_ptr->image.rows > 60 && cv_ptr->image.cols > 60)
-            cv::circle(cv_ptr->image, cv::Point(50, 50), 10, CV_RGB(255, 0, 0));
+        // if (cv_ptr->image.rows > 60 && cv_ptr->image.cols > 60)
+        //     cv::circle(cv_ptr->image, cv::Point(50, 50), 10, CV_RGB(255, 0, 0));
 
         // std_msgs::String debugMsg;
         // debugMsg.data = "teste";
         // pubDebug.publish(debugMsg);
 
         // Update GUI Window
-        cv::imshow(OPENCV_WINDOW, cv_ptr->image);
-        cv::waitKey(3);
 
-        // Output modified video stream
-        image_pub_.publish(cv_ptr->toImageMsg());
+        cv_ptr.read(imgTmp);
+
+        Mat imgLines = Mat::zeros( imgTmp.size(), CV_8UC3 );
+
+        while (true) {
+            Mat imgOriginal;
+
+            bool bSuccess = cv_ptr.read(imgOriginal); // read a new frame from video
+
+
+            if (!bSuccess) //if not success, break loop
+            {
+                cout << "Cannot read a frame from video stream" << endl;
+                break;
+            }
+
+            Mat imgHSV;
+
+            cvtColor(imgOriginal, imgHSV, COLOR_BGR2HSV); //Convert the captured frame from BGR to HSV
+ 
+            Mat imgThresholded;
+
+            inRange(imgHSV, Scalar(iLowH, iLowS, iLowV), Scalar(iHighH, iHighS, iHighV), imgThresholded); //Threshold the image
+      
+            //morphological opening (removes small objects from the foreground)
+            erode(imgThresholded, imgThresholded, getStructuringElement(MORPH_ELLIPSE, Size(5, 5)) );
+            dilate( imgThresholded, imgThresholded, getStructuringElement(MORPH_ELLIPSE, Size(5, 5)) ); 
+
+            //morphological closing (removes small holes from the foreground)
+            dilate( imgThresholded, imgThresholded, getStructuringElement(MORPH_ELLIPSE, Size(5, 5)) ); 
+            erode(imgThresholded, imgThresholded, getStructuringElement(MORPH_ELLIPSE, Size(5, 5)) );
+
+            //Calculate the moments of the thresholded image
+            Moments oMoments = moments(imgThresholded);
+
+            double dM01 = oMoments.m01;
+            double dM10 = oMoments.m10;
+            double dArea = oMoments.m00;
+
+            // if the area <= 10000, I consider that the there are no object in the image and it's because of the noise, the area is not zero 
+            if (dArea > 10000){
+                //calculate the position of the ball
+                int posX = dM10 / dArea;
+                int posY = dM01 / dArea;        
+                        
+                if (iLastX >= 0 && iLastY >= 0 && posX >= 0 && posY >= 0)
+                {
+                    //Draw a red line from the previous point to the current point
+                    line(imgLines, Point(posX, posY), Point(iLastX, iLastY), Scalar(0,0,255), 2);
+                }
+
+                iLastX = posX;
+                iLastY = posY;
+            }
+
+            imshow("Thresholded Image", imgThresholded); //show the thresholded image
+
+            imgOriginal = imgOriginal + imgLines;
+            imshow("Original", imgOriginal); //show the original image
+
+            if (waitKey(30) == 27) //wait for 'esc' key press for 30ms. If 'esc' key is pressed, break loop
+            {
+                cout << "esc key is pressed by user" << endl;
+                break; 
+            }
+        }
+
+        // cv::imshow(OPENCV_WINDOW, cv_ptr->image);
+        // cv::waitKey(3);
+
+        // // Output modified video stream
+        // image_pub_.publish(cv_ptr->toImageMsg());
     }
 };
 
