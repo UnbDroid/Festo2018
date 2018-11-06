daa8f1e3480aff794e541d289fd43ad3354dab74
Matheus Feitosa de Castro <matheus@Matheuss-MacBook-Air.local>
2018-11-05 21:51:13 -0300

    add teste com opencv

--- catkin_festo2018/src/vision/src/vision_node.cpp
+++ catkin_festo2018/src/vision/src/vision_node.cpp
@@ -78,17 +78,18 @@ class ImageConverter {
 
         Mat imgLines = Mat::zeros( imgTmp.size(), CV_8UC3 );;
         // Testado
-        while (true){
+
+        while(true){
             Mat imgOriginal;
 
             Mat imgHSV;
 
-            cvtColor(imgOriginal, imgHSV, COLOR_BGR2HSV);
-
+            cvtColor(imgOriginal, imgHSV, COLOR_BGR2HSV); //Convert the captured frame from BGR to HSV
+ 
             Mat imgThresholded;
 
             inRange(imgHSV, Scalar(iLowH, iLowS, iLowV), Scalar(iHighH, iHighS, iHighV), imgThresholded); //Threshold the image
-      
+            
             //morphological opening (removes small objects from the foreground)
             erode(imgThresholded, imgThresholded, getStructuringElement(MORPH_ELLIPSE, Size(5, 5)) );
             dilate( imgThresholded, imgThresholded, getStructuringElement(MORPH_ELLIPSE, Size(5, 5)) ); 
@@ -97,48 +98,8 @@ class ImageConverter {
             dilate( imgThresholded, imgThresholded, getStructuringElement(MORPH_ELLIPSE, Size(5, 5)) ); 
             erode(imgThresholded, imgThresholded, getStructuringElement(MORPH_ELLIPSE, Size(5, 5)) );
 
-            //Calculate the moments of the thresholded image
-            Moments oMoments = moments(imgThresholded);
 
-            double dM01 = oMoments.m01;
-            double dM10 = oMoments.m10;
-            double dArea = oMoments.m00;
-
-            // if the area <= 10000, I consider that the there are no object in the image and it's because of the noise, the area is not zero 
-            if (dArea > 10000)
-            {
-                //calculate the position of the ball
-                int posX = dM10 / dArea;
-                int posY = dM01 / dArea;        
-                        
-                if (iLastX >= 0 && iLastY >= 0 && posX >= 0 && posY >= 0)
-                {
-                    //Draw a red line from the previous point to the current point
-                    line(imgLines, Point(posX, posY), Point(iLastX, iLastY), Scalar(0,0,255), 2);
-                }
-
-                iLastX = posX;
-                iLastY = posY;
-            }
-
-            imshow("Thresholded Image", imgThresholded); //show the thresholded image
-
-            imgOriginal = imgOriginal + imgLines;
-            imshow("Original", imgOriginal); //show the original image
-
-            if (waitKey(30) == 27) //wait for 'esc' key press for 30ms. If 'esc' key is pressed, break loop
-            {
-                cout << "esc key is pressed by user" << endl;
-                break; 
-            }
-    
-
-        //cv::imshow(OPENCV_WINDOW, imgTmp);
-        cv::waitKey(3);
-
-        // Output modified video stream
-        image_pub_.publish(cv_ptr->toImageMsg());
-    }
+        }
     }
 };
 
